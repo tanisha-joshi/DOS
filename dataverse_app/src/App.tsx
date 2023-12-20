4a358d0977c3a4f0d079b94ce1ec60a32a39308d
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-12-20 12:05:26 +0530

    connect dataverse wallet

--- dataverse_app/src/App.tsx
+++ dataverse_app/src/App.tsx
@@ -28,13 +28,28 @@ import Steps from "./components/Steps";
 import Features from "./components/Features";
 
 
+const modelParser = new ModelParser(app as Output);
 export default function App(){
 
+  const { connectApp } = useApp({
+
+
+    appId: modelParser.appId,
+    autoConnect: false,
+    onSuccess: result => {
+      console.log("[connect]connect app success, result:", result);
+    },
+  });
+
+const handle = ()=>{
+  connectApp()
+}
   
   return (
     <div className=" text-white m-0 p-0 bg-[#070e1c] min-h-screen box-border flex flex-col">
       
-      <Navbar />
+      <Navbar handleFunction= {handle} />
+      {/* <button onClick={handle}>connect</button> */}
       <Landing />
       <Features />
       <Steps />
--- dataverse_app/src/components/Navbar.tsx
+++ dataverse_app/src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useStore } from "@dataverse/hooks";
+export default function Navbar({handleFunction}:any) {
+  const { pkh} = useStore();
 
-export default function Navbar() {
   return (
-    <nav className="fixed  z-[9999999] w-full flex h-16 px-4   items-center bg-[#061f30] left-0 top-0">
+    <nav className="fixed z-[9999999] w-full flex h-16 px-4 justify-between  items-center bg-[#061f30] left-0 top-0">
         <div className="text-2xl h-min">Gat3r</div>
+        <button className="px-8 rounded-md py-3 bg-[#40a2db]"  onClick={handleFunction}>{pkh?pkh:"connect"}</button>
     </nav>
   );
 }
