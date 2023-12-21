a7ba52eb9c661d03b9f64475d36764e28813adc4
Samarth Saxena <samarthsaxena1672003@gmail.com>
2023-12-21 21:25:32 +0530

    links added in navbar

--- dataverse_app/src/components/Navbar.tsx
+++ dataverse_app/src/components/Navbar.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useStore } from "@dataverse/hooks";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-export default function Navbar({handleFunction}:any) {
-  const { pkh} = useStore();
+export default function Navbar({ handleFunction }: any) {
+  const { pkh } = useStore();
 
   return (
     <nav className="fixed z-[9999999] w-full flex h-16 px-4 opacity-95 justify-between  items-center bg-[#061f30] left-0 top-0">
-        <div className="text-2xl h-min">Gat3r</div>
-        <ConnectButton/>
-        {/* <button className="px-8 rounded-md py-3 bg-[#40a2db]"  onClick={handleFunction}>{pkh?pkh:"connect"}</button> */}
+      <div className="text-2xl h-min">Gat3r</div>
+      <div className="flex gap-12 h-min">
+       <div className="h-min self-center"> <a href="/createCommunity">Create</a></div>
+        <div className="h-min self-center"><a href="/postsPage">Explore</a></div>
+        <ConnectButton />
+      </div>
+      {/* <button className="px-8 rounded-md py-3 bg-[#40a2db]"  onClick={handleFunction}>{pkh?pkh:"connect"}</button> */}
     </nav>
   );
 }
